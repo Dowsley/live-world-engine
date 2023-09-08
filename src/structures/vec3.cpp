9d75186f60765cca456be3a062b29619e5bd50e2
Dowsley <joao.dowsley850@gmail.com>
2023-09-08 00:54:32 -0300

    Add Vec3 definition

---
+++ src/structures/vec3.cpp
@@ -0,0 +1,68 @@
+#include "vec3.h"
+
+#include <cmath>
+
+Vec3::Vec3()
+{
+   e[0] = 0;
+   e[1] = 0;
+   e[2] = 0;
+}
+
+Vec3::Vec3(int x, int y, int z)
+{
+    e[0] = x;
+    e[1] = y;
+    e[2] = z;
+}
+
+int Vec3::x() const { return e[0]; }
+int Vec3::y() const { return e[1]; }
+int Vec3::z() const { return e[2]; }
+
+void Vec3::setX(int x) { e[0] = x; }
+void Vec3::setY(int y) { e[1] = y; }
+void Vec3::setZ(int z) { e[2] = z; }
+
+Vec3 Vec3::operator-() const { return Vec3(-e[0], -e[1], -e[2]); }
+int Vec3::operator[](int i) const { return e[i]; }
+int& Vec3::operator[](int i) { return e[i]; }
+
+Vec3& Vec3::operator+=(const Vec3 &v)
+{
+    e[0] += v[0];
+    e[1] += v[1];
+    e[2] += v[2];
+    return *this;
+}
+
+Vec3& Vec3::operator+=(const int t)
+{
+    e[0] += t;
+    e[1] += t;
+    e[2] += t;
+    return *this;
+}
+
+Vec3& Vec3::operator*=(const int t)
+{
+    e[0] *= t;
+    e[1] *= t;
+    e[2] *= t;
+    return *this;
+}
+
+Vec3& Vec3::operator/=(const int t)
+{
+    return *this *= 1/t;
+}
+
+Vec3 operator+(const Vec3 &u, const Vec3 &v) { return Vec3(u[0] + v[0], u[1] + v[1], u[2] + v[2]); }
+Vec3 operator+(const Vec3 &u, int val) { return Vec3(u[0] + val, u[1] + val, u[2] + val); } 
+Vec3 operator+(int val, const Vec3 &u) { return u + val; } 
+Vec3 operator-(const Vec3 &u, const Vec3 &v) { return Vec3(u[0] - v[0], u[1] - v[1], u[2] - v[2]); }
+Vec3 operator*(const Vec3 &u, const Vec3 &v) { return Vec3(u[0] * v[0], u[1] * v[1], u[2] * v[2]); }
+Vec3 operator*(int t, const Vec3 &v) { return Vec3(t*v[0], t*v[1], t*v[2]); }
+Vec3 operator*(const Vec3 &v, int t) { return t * v; }
+Vec3 operator/(Vec3 v, int t) { return (1/t) * v; }
+std::ostream& operator<<(std::ostream &out, const Vec3 &v) { return out << "Vec3(" << v[0] << ' ' << v[1] << ' ' << v[2] << ")"; }
---
+++ src/structures/vec3.h
@@ -0,0 +1,45 @@
+#ifndef VEC3_H
+#define VEC3_H
+
+#include <fstream>
+
+class Vec3
+{
+    public:
+        Vec3();
+        Vec3(int x, int y, int z);
+
+        // No, I'm not gonna respect standard names here.
+        int x() const;
+        int y() const;
+        int z() const;
+
+        // here I will
+        void setX(int x);
+        void setY(int y);
+        void setZ(int z);
+
+        Vec3 operator-() const;
+        int operator[](int i) const;
+        int& operator[](int i);
+
+        Vec3& operator+=(const Vec3 &v);
+        Vec3& operator+=(const int t);
+        Vec3& operator*=(const int t);
+        Vec3& operator/=(const int t);
+
+        friend Vec3 operator+(const Vec3 &u, const Vec3 &v);
+        friend Vec3 operator+(const Vec3 &u, int val);
+        friend Vec3 operator+(int val, const Vec3 &u);
+        friend Vec3 operator-(const Vec3 &u, const Vec3 &v);
+        friend Vec3 operator*(const Vec3 &u, const Vec3 &v);
+        friend Vec3 operator*(int t, const Vec3 &v);
+        friend Vec3 operator*(const Vec3 &v, int t);
+        friend Vec3 operator/(Vec3 v, int t);
+        friend std::ostream& operator<<(std::ostream &out, const Vec3 &v);
+
+    protected:
+        int e[3];
+};
+
+#endif
